361d5cfa46862930b5389a708a8e1aa6b1656e14
josi-lima <teacher.josi@yahoo.com>
2022-09-16 00:51:06 -0300

    update js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// Object with music sounds
+
+const sounds = {
+  'A': 'sound_boom.wav',
+  'S': 'sound_clap.wav',
+  'D': 'sound_hihat.wav',
+  'F': 'sound_kick.wav',
+  'G': 'sound_openhat.wav',
+  'H': 'sound_ride.wav',
+  'J': 'sound_snare.wav',
+  'K': 'sound_kalimba.wav',
+  'L': 'sound_tink.wav'
+}
+
+// function to create the music-key buttons
+
 const createDiv = (text) => {
   const div = document.createElement('div');
   div.classList.add('key');
